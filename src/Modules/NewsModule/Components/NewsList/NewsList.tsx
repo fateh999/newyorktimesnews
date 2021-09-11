009c6049ce4021661dedbdf9d781c299e8f5551e
Fateh Farooqui <farooquifateh184@gmail.com>
2021-09-11 13:22:18 +0530

    Fixes

--- src/Modules/NewsModule/Components/NewsList/NewsList.tsx
+++ src/Modules/NewsModule/Components/NewsList/NewsList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import {FlatList} from 'react-native';
 import styled from 'styled-components/native';
 import useKeywordsValue from '../../Hooks/useKeywordsValue';
@@ -10,18 +10,35 @@ const Spacer = styled.View`
   height: 15px;
 `;
 
+const NoDataFound = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const NoDataFoundText = styled.Text`
+  font-size: 20px;
+  color: white;
+`;
+
 function NewsList() {
   const {results, isLoading, refetch} = useNewsQuery();
   const location = useLocationValue();
   const keywords = useKeywordsValue();
-
-  return (
-    <FlatList
-      data={results.filter(
+  const finalResults = useMemo(
+    () =>
+      results.filter(
         result =>
           (result.des_facet.indexOf(keywords) !== -1 || keywords === '') &&
           (result.geo_facet.indexOf(location) !== -1 || location === ''),
-      )}
+      ),
+    [keywords, location, results],
+  );
+  const contentContainerStyle = finalResults.length > 0 ? {} : {flex: 1};
+
+  return (
+    <FlatList
+      data={finalResults}
       renderItem={({item}: any) => <NewsItem {...item} />}
       ItemSeparatorComponent={() => <Spacer />}
       ListHeaderComponent={() => <Spacer />}
@@ -29,6 +46,12 @@ function NewsList() {
       showsVerticalScrollIndicator={false}
       refreshing={isLoading}
       onRefresh={refetch}
+      contentContainerStyle={contentContainerStyle}
+      ListEmptyComponent={() => (
+        <NoDataFound>
+          {!isLoading && <NoDataFoundText>No Data Found</NoDataFoundText>}
+        </NoDataFound>
+      )}
     />
   );
 }
--- src/Utils/Helpers.ts
+++ src/Utils/Helpers.ts
@@ -1,15 +1,26 @@
 import axios from 'axios';
 import {API_URL} from './Constants';
 import UltimateConfig from 'react-native-ultimate-config';
+import Snackbar from 'react-native-snackbar';
+import ThemeService from 'src/Modules/ThemeModule/Services/ThemeService';
 
 export const fetcher = async (url: string) => {
   return axios.get(`${API_URL}${url}?api-key=${UltimateConfig.API_KEY}`);
 };
 
 export const onError = (error: any) => {
+  const theme = ThemeService.theme$.getValue();
   if (error?.response) {
     console.log({error: error?.response});
+    Snackbar.show({
+      text: 'Something went wrong, Please retry',
+      backgroundColor: theme.colors.error,
+    });
   } else {
+    Snackbar.show({
+      text: 'Failed to connect with server',
+      backgroundColor: theme.colors.error,
+    });
     console.log({error});
   }
 };
